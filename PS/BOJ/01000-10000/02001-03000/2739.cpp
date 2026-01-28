3612e47176ee09bd8b4d2d3886504f0cea531c03
KimJongHyun2 <poreer0597@hs.ac.kr>
2026-01-28 17:39:41 +0900

    feat: cpp 2회차

---
+++ PS/BOJ/01000-10000/02001-03000/2739.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+   int result = 0;
+
+   cin >> n;
+   
+   for(int i = 1; i <=9; i++){
+        cout << n << " * " << i << " = " << n*i << "\n";
+
+   }
+   return 0;
+
+
+
+}
